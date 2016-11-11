536020dc2e142152f1bac0e0609038709a2993d8
vasya <merkure74@mail.ru>
2016-11-11 20:43:35 +0600

    dz 2c

--- src/index.js
+++ src/index.js
@@ -3,10 +3,39 @@ import cors from 'cors';
 
 const app = express();
 app.use(cors());
+
 app.get('/', (req, res) => {
-  res.json({
-    hello: 'JS World',
-  });
+
+	var str = req.query.username;
+	str = str.replace(/@/g, '');
+	var reg = /(@+)?(http:|https:)?(\/\/)?([^\/?]+\/)?([^\/?&@]+)/i;
+	var nickname = str.match(reg);
+	console.log(nickname);
+
+	if ( nickname == null )
+	{
+		nickname = 'Invalid username';
+	}
+	else
+	{
+		nickname = nickname[5];
+	}
+
+	var reg2 = /(www)|\d+\.|(ru)|(com)/i;
+	var test = reg2.test(nickname);
+	if ( test )
+	{
+		nickname = 'Invalid username';
+	}
+
+	if ( nickname )
+	{
+		nickname = `@${nickname}`;
+	}
+	console.log(nickname);
+
+  res.send(nickname);
+
 });
 
 app.listen(3000, () => {
